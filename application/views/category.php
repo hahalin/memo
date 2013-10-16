d2592b111f50d3e5f365d23d498ace6bfcec3109
unknown <frank@mis-7742.masterlink.com.tw>
2013-10-16 18:42:40 +0800

    10/16 commit

---
+++ application/views/category.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap 101 Template</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="<?php echo base_url()?>assets/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<link href="<?php echo base_url()?>assets/css/slidingmenu.css" rel="stylesheet" media="screen">
+
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<script></script>
+				<div class="span9 row">
+					<div class="span3">
+						<div class="slashc-sliding-menu" id="inbox-menu-js" style="overflow: display; width: 249px; height: 518px;">	
+													<h1 id="inbox-menu-header-js" >
+														<span style="opacity: 1;">test</span> 
+														<a title="Slide Back Home" href="javascript:void(0);" class="slashc-sliding-menu-home btn btn-inverse">
+															<i class="icon-angle-left"></i>
+														</a>
+													</h1>
+													<a title="Slide Back Home" href="javascript:void(0);" id="slashc-sliding-menu-back btn" 
+														  style="left:50px;"
+														>
+															back
+													</a>
+
+													<ul class="mailbox" id="mailbox-js" style="height: 622px; top: 0px; left: 0px;">
+														<li><a href="javascript:void(0);"><span>Inbox</span><span class="badge">26</span><i class="icon-angle-right"></i></a>
+															<ul style="left: 249px; top: 0px; display: block;">
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			From: David Simpson
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Dear Victoria, Congratulations! Your work has been uploaded to wrapbootstrap.com...</span>
+																		</span>
+																	</a>
+																	<ul>
+																		<li>
+																			<a href='#'><span>aaa</span></a>
+																		</li>
+																	</ul>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Re:Last Year sales
+																			<i class="icon-paper-clip"></i>
+																			<span class="tiny-des">Hey Vicky, find attached! Thx :-)</span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Company Party
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Hi, You have been cordially invited to join new year after party.</span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			RE: 2 Bugs found...
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">I have found two more bugs in this your beta version, maybe its not working...</span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			2 Bugs found...
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales.</span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Incka porfa varos?
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Forut dashly kuba tuma lope hamur toaila... </span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			This is a test message
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Laku naka obaba muta...</span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Welcome to Jarvis!
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Feugiat a, tellus. Phasellus viverra nulla ut metus varius. Quisque rutrum. Aenean imperdiet... </span>
+																		</span>
+																	</a>
+																</li>
+																<li>
+																	<div class="push-down"></div>
+																</li>
+															</ul>
+														</li>
+														<li>
+															<a href="javascript:void(0);"><span>Outbox</span><span class="badge">1</span><i class="icon-angle-right"></i></a>
+															<ul style="left: 249px; top: -44px; display: none;">
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Message Title
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Message line truncated...</span>
+																		</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+														<li>
+															<a href="javascript:void(0);"><span>Sent</span><i class="icon-angle-right"></i></a>
+															<ul style="left: 249px; top: -88px; display: none;">
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Message Title
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Message line truncated...</span>
+																		</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+														<li>
+															<a href="javascript:void(0);"><span>Trash</span><i class="icon-angle-right"></i></a>
+															<ul style="left: 249px; top: -132px; display: none;">
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Message Title
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Message line truncated...</span>
+																		</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+														<li>
+															<a href="javascript:void(0);"><span>Saved Messages</span><i class="icon-angle-right"></i></a>
+															<ul style="left: 249px; top: -176px; display: none;">
+																<li>
+																	<a href="javascript:void(0)">
+																		<span class="tiny-sub">
+																			Message Title
+																			<!--<i class="icon-paper-clip"></i>-->
+																			<span class="tiny-des">Message line truncated...</span>
+																		</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+						
+					</div>
+					<div class="span3">
+						<div class="slashc-sliding-menu" id="menua">
+							<h1 id="inbox-menu-header-js"><span>test</span><a title="Slide Back Home" href="javascript:void(0);" class="slashc-sliding-menu-home btn btn-inverse"> <i class="icon-angle-left"></i> </a></h1>
+							<ul >
+								<li>
+									<a href="javascript:void(0);" tag=1><span>aa</span></a>
+									<ul>
+										<li>
+											<a href="javascript:void(0);" tag=11><span>aa1</span></a>
+											<ul>
+												<li>
+													<a href="javascript:void(0);" tag=12><span>aa21</span></a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="javascript:void(0);" tag=12><span>aa2</span></a>
+											<ul>
+												<li>
+													<a href="javascript:void(0);" tag=12><span>aa21</span></a>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="javascript:void(0);" tag=2><span>bb</span></a>
+								</li>
+
+							</ul>
+						</div>
+
+					</div>
+					<div class="span3">
+						<div class="btn-group">
+							<button class="btn btn-primary">
+								Action
+							</button>
+							<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+								<span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu">
+								<li>
+									<a href=#>setting</a>
+								</li>
+								<li id="rfq">
+									<a href=#>rfq<span class="badge">1xx</span></a>
+								</li>
+							</ul>
+						</div>
+						<span id="loading">loading...</span>
+					</div>
+				</div>
+				<div class="span9">
+					<div class="hero-unit">
+						hero unit
+					</div>
+					<div id="listgrid" class="row-fluid">
+						<div class="span4">
+							<p>
+								aaa
+							</p>
+							<p>
+								<a class="btn" href=# pid="1">a1</a>
+							</p>
+						</div>
+						<div class="span4">
+							<p>
+								bb
+							</p>
+							<p>
+								<a class="btn" href=# pid="2">b1</a>
+							</p>
+						</div>
+						<div class="span4">
+							<p>
+								cc
+							</p>
+							<p>
+								<a class="btn" href=# pid="3">c1</a>
+							</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+		<script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url()?>assets/js/slashc-sliding-menu.js"></script>
+		<script src="<?php echo base_url()?>assets/js/slimScroll.min.js"></script>
+
+		<script>
+			function getcount() {
+				$.get('app/getcount', {
+					para1 : 1
+				}, function(data, textStatus) {
+					//alert(data);
+					$('#rfq a span.badge').text(data);
+					$('#loading').hide();
+				}
+				//,"json"
+				);
+			}
+
+			function hide_inbox_menu_header() {
+				$('#inbox-menu-header-js').animate({
+					"top" : "-42px"
+				}, 250);
+				$('#menua ul').animate({
+					"top" : "-42px"
+				}, 250);
+			}
+
+			function show_inbox_menu_header() {
+				$('#inbox-menu-header-js').animate({
+					"top" : "0px"
+				}, 250);
+				$('#menua ul').animate({
+					"top" : "0px"
+				}, 250);
+			}
+
+			$(function() {
+
+				//alert('jquery');
+				$('#loading').hide();
+
+				$('#loading').show();
+
+				getcount();
+
+				$('#listgrid a.btn').click(function() {
+					$('#loading').hide();
+					$.post('app/add', {
+						'pid' : $(this).attr('pid')
+					}).done(function(data) {
+						//alert(data);
+						var o = $.parseJSON(data);
+						if (!o.success) {
+							alert(o.msg);
+						}
+						//alert('done');
+						getcount();
+					}, "json");
+
+				});
+
+				$('#menua a.slashc-sliding-menu-home').click(function() {
+					//hide_inbox_menu_header();
+				});
+				$('#menua >ul > li > a').click(function(event) {
+					//$('#menua a.slashc-sliding-menu-home').text($(this)
+					//alert($(this).parent().find('ul li').size());
+					show_inbox_menu_header();
+				});
+
+				return;
+				$('#menua').slimScroll({
+					height : '518px',
+					width : '249',
+					disableFadeOut : true,
+					distance : 3,
+					size : 7
+				});
+				hide_inbox_menu_header();
+
+				/*
+				 $('#inbox-loading-panel-js').slimScroll({
+				 height : '518px',
+				 width : 'auto',
+				 disableFadeOut : true,
+				 distance : 5,
+				 size : 7
+				 });*/
+			});
+		</script>
+
+	</body>
+</html>
